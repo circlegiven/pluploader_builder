4c8b6cdd26f37e14cff413d49cb8d89ca5f1be47
circlegiven <circlegiven@gmail.com>
2019-04-17 18:54:16 +0900

    Add pluploader demo file
    - used angular

---
+++ pluploader.demo.ts
@@ -0,0 +1,31 @@
+import {PluploaderOptions} from "./pluploader.options";
+import {ElementRef, ViewChild} from "@angular/core";
+
+declare let pluploader;
+
+export class PluploaderDemo {
+
+  @ViewChild('testElement')
+  private _testElement: ElementRef;
+
+  public chunk_uploader;
+
+  testInit() {
+    this.chunk_uploader = pluploader.Uploader(new PluploaderOptions.OptionsBuilder()
+      .Runtimes('html5')
+      .ChunkSize(0)
+      .BrowseButton(this._testElement.nativeElement)
+      .Url('test.test.test')
+      .Headers({
+        'Accept': 'application/json, text/plain, */*',
+        'Authorization': 'TOKEN'
+      })
+      .Filters(new PluploaderOptions.FileFiltersBuilder()
+        .MaxFileSize(0)
+        .PreventDuplicates(true)
+        .builder()
+      )
+      .build()
+    );
+  }
+}
